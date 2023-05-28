88562e27de4007a2d60ae7f0fb743d69d03a2a9c
Yuishi1225 <y.kuni.23.4.927@4nonome.com>
2023-05-29 00:25:17 +0900

    '修正'

--- pi.py
+++ pi.py
@@ -5,3 +5,8 @@ text = """
 
 # TODO
 
+text = ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics","All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard" ]
+
+list(map(len, text))
+
+list(map(len, ["a", "bc", "def"]))
