6755df2f0d40b7bfd3ea6ca6e2e8af18d565b73a
Yuishi1225 <y.kuni.23.4.927@4nonome.com>
2023-07-22 19:22:08 +0900

    hennkou

--- euclid.py
+++ euclid.py
@@ -1,16 +1,20 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a =int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 
 # TODO
 
-def gcd(c,d):
-    while d != 0:
-        c, d= d, c%d
+def gcd(a,b):
+    while b != 0:
+        a, b= a, a%b
     print(c)
+    
 
 gcd(10, 20)
 gcd(14, 91)
 gcd(91, 14)
+
+#問4
+
 
 
 
