4325db95e41f20fa1acfb29c1082b05c089e9c57
Yuishi1225 <y.kuni.23.4.927@4nonome.com>
2023-07-22 22:18:12 +0900

    finished

--- prime_number.py
+++ prime_number.py
@@ -1,37 +1,18 @@
-a = input("aの値を入力:61")
-b = input("bの値を入力:10")
+a = int(input("aの値を入力:"))
 
 # TODO
-n=61
-def is_prime(n):
-    if n <1:
+def is_prime(a):
+    if a <2:
         return False
     else:
-        for i in range(2, n):
-            if n%i==0:
+        for i in range(2, a):
+            if a%i==0:
                 return False
     return True
 
-if is_prime(n):
-    print("素数です")
-else:
-    print("素数ではありません")
+result=is_prime(a)
+print(result)
 
-#2
-n=91
-def is_prime(n):
-    if n <1:
-        return False
-    else:
-        for i in range(2, n):
-            if n%i==0:
-                return False
-    return True
-
-if is_prime(n):
-    print("素数です")
-else:
-    print("素数ではありません")
 
 
 
